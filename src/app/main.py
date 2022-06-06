47bd41e1c6c12eb32042accaac3326bd65663968
Peter Schutt <peter@topsport.com.au>
2022-06-06 15:54:25 +1000

    Improve output.

--- src/app/main.py
+++ src/app/main.py
@@ -8,16 +8,29 @@ logging.basicConfig(level=config.LOG_LEVEL)
 
 
 n = 100000
-ps = [.6, .4, .3, .3, .4, .1, .3, .3]
+ps = [
+    0.45,
+    0.44,
+    0.37,
+    0.37,
+]
 
 
 def main() -> str:
+    rng = np.random.default_rng(654654)
     probs = np.array(ps)
+    print(f"Inputs: {probs}")
     # rands = np.random.random(len(ps))
-    rands = np.random.random((n, len(ps)))
-    sim = (rands <= probs)
+    rands = rng.random((n, len(ps)))
+    sim = rands <= probs
     summed_sim = sim.sum(axis=1)
     unique, cnt = np.unique(summed_sim, return_counts=True)
+    exact_summed = 0.0
     for elem in unique:
-        count = (summed_sim >= elem).sum()
-        print(elem, count / n)
+        exact_count = (summed_sim == elem).sum()
+        exact_or_more_count = (summed_sim >= elem).sum()
+        exact_prob = exact_count / n
+        exact_or_more_prob = exact_or_more_count / n
+        print(f"Exactly {elem}: {exact_prob} | {elem} or more: {exact_or_more_prob}")
+        exact_summed += exact_prob
+    assert np.isclose(exact_summed, 1)
